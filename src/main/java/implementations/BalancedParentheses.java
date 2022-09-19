a8bfcc78c27e25bbf730d3d207b01d209e27c4d3
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-19 20:02:47 +0300

    added new exercise

--- src/main/java/implementations/BalancedParentheses.java
+++ src/main/java/implementations/BalancedParentheses.java
@@ -2,6 +2,9 @@ package implementations;
 
 import interfaces.Solvable;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayDeque;
+
 public class BalancedParentheses implements Solvable {
     private String parentheses;
 
@@ -11,6 +14,36 @@ public class BalancedParentheses implements Solvable {
 
     @Override
     public Boolean solve() {
-        return null;
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < parentheses.length(); i++) {
+            char c = parentheses.charAt(i);
+            if ("({[".contains(c + "")) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (c == '}') {
+                    if (stack.peek() == '{') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (c == ']') {
+                    if (stack.peek() == '[') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (c == ')') {
+                    if (stack.peek() == '(') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
