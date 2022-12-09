67f02e14807d6038f3dac806442d26f3b7b1a226
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-12-09 12:10:50 +0200

    add new exam

--- 1. VaccOps_Resources/src/vaccopsjava/Main.java
+++ 1. VaccOps_Resources/src/vaccopsjava/Main.java
@@ -1,7 +1,36 @@
 package vaccopsjava;
 
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
+        VaccOps vaccOps = new VaccOps();
 
+        Doctor doctor1 = new Doctor("Pesho", 10);
+        Doctor doctor2 = new Doctor("Gosho", 100);
+        Doctor doctor3 = new Doctor("Tosho", 1);
+        Doctor doctor4 = new Doctor("Momchil", 1);
+        Doctor doctor5 = new Doctor("Krasi", 1);
+
+        Patient patient1 = new Patient("Lora", 168, 25, "Sofia");
+        Patient patient2 = new Patient("Nora", 168, 14, "Sofia");
+        Patient patient3 = new Patient("Vqra", 175, 22, "Pleven");
+        Patient patient4 = new Patient("Magi", 180, 39, "Burgas");
+        Patient patient5 = new Patient("Viki", 160, 65, "Pleven");
+
+
+        System.out.println("=====================");
+        vaccOps.addDoctor(doctor1);
+        vaccOps.addDoctor(doctor2);
+        vaccOps.addDoctor(doctor3);
+        vaccOps.addPatient(doctor1,patient1);
+        vaccOps.addPatient(doctor1,patient2);
+        vaccOps.addPatient(doctor2,patient3);
+        vaccOps.addPatient(doctor2,patient4);
+        vaccOps.addPatient(doctor3,patient5);
+
+
+        Collection<Patient> sorted = vaccOps.getPatientsSortedByDoctorsPopularityAscThenByHeightDescThenByAge();
+        System.out.println("=====================");
     }
 }
--- 1. VaccOps_Resources/src/vaccopsjava/VaccOps.java
+++ 1. VaccOps_Resources/src/vaccopsjava/VaccOps.java
@@ -1,60 +1,122 @@
 package vaccopsjava;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class VaccOps implements IVaccOps {
+    Map<String, Doctor> doctorMap;
+    Map<String, Patient> patientMap;
+    Map<Patient, Doctor> patientWithDoctorMap;
+    Map<Doctor, List<Patient>> doctorWithPatientsListMap;
+
 
     public VaccOps() {
+        doctorMap = new TreeMap<>();
+        patientMap = new HashMap<>();
+        patientWithDoctorMap = new HashMap<>();
+        doctorWithPatientsListMap = new HashMap<>();
     }
 
     public void addDoctor(Doctor d) {
+        String name = d.name;
+        if (doctorMap.containsKey(name)) {
+            throw new IllegalArgumentException();
+        }
+        doctorMap.put(name, d);
+        doctorWithPatientsListMap.put(d, new ArrayList<>());
     }
 
     public void addPatient(Doctor d, Patient p) {
+        String name = d.name;
+        if (!doctorMap.containsKey(name)||patientMap.containsKey(p.name)) {
+            throw new IllegalArgumentException();
+        }
+        patientMap.put(p.name, p);
+        patientWithDoctorMap.put(p, d);
+        doctorWithPatientsListMap.get(d).add(p);
     }
 
     public Collection<Doctor> getDoctors() {
-        return null;
+        return new ArrayList<>(doctorMap.values());
     }
 
     public Collection<Patient> getPatients() {
-        return null;
+        return new ArrayList<>(patientMap.values());
     }
 
     public boolean exist(Doctor d) {
-        return false;
+        return doctorMap.containsKey(d.name);
     }
 
     public boolean exist(Patient p) {
-        return false;
+        return patientMap.containsKey(p.name);
     }
 
 
     public Doctor removeDoctor(String name) {
-        return null;
+        if (!doctorMap.containsKey(name)) {
+            throw new IllegalArgumentException();
+        }
+        Doctor doctor = doctorMap.remove(name);
+        for (Patient patient : doctorWithPatientsListMap.get(doctor)) {
+            patientMap.remove(patient.name);
+            patientWithDoctorMap.remove(patient);
+        }
+        doctorWithPatientsListMap.remove(doctor);
+        return doctor;
     }
 
     public void changeDoctor(Doctor from, Doctor to, Patient p) {
+        if (!doctorMap.containsKey(from.name) || !doctorMap.containsKey(to.name) || !patientMap.containsKey(p.name)) {
+            throw new IllegalArgumentException();
+        }
+        doctorWithPatientsListMap.get(from).remove(p);
+        doctorWithPatientsListMap.get(to).add(p);
     }
 
     public Collection<Doctor> getDoctorsByPopularity(int populariry) {
-        return null;
+        return doctorMap.values()
+                .stream()
+                .filter(doctor -> doctor.popularity == populariry)
+                .collect(Collectors.toList());
     }
 
     public Collection<Patient> getPatientsByTown(String town) {
-        return null;
+        return patientMap.values()
+                .stream()
+                .filter(patient -> patient.town.equals(town))
+                .collect(Collectors.toList());
     }
 
     public Collection<Patient> getPatientsInAgeRange(int lo, int hi) {
-        return null;
+        return patientMap.values()
+                .stream()
+                .filter(patient -> patient.age >= lo && patient.age <= hi)
+                .collect(Collectors.toList());
     }
 
     public Collection<Doctor> getDoctorsSortedByPatientsCountDescAndNameAsc() {
-        return null;
+        return doctorMap.values()
+                .stream()
+                .sorted((f, s) -> Integer.compare(doctorWithPatientsListMap.get(s).size(), doctorWithPatientsListMap.get(f).size()))
+                .collect(Collectors.toList());
     }
 
     public Collection<Patient> getPatientsSortedByDoctorsPopularityAscThenByHeightDescThenByAge() {
-        return null;
+        return patientWithDoctorMap.entrySet()
+                .stream()
+                .sorted((f, s) -> {
+                    int result = f.getValue().popularity-s.getValue().popularity;
+                    if (result == 0){
+                        result = s.getKey().height - f.getKey().height;
+                        if (result == 0){
+                            result = f.getKey().age - s.getKey().age;
+                        }
+                    }
+                    return result;
+                })
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
 }
