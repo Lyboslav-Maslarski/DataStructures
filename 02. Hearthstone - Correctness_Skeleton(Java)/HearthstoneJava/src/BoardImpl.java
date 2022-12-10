a5c9bb1904f9c0de0a2b607dac0b7832831ede29
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-12-10 18:06:59 +0200

    add new exam

--- 02. Hearthstone - Correctness_Skeleton(Java)/HearthstoneJava/src/BoardImpl.java
+++ 02. Hearthstone - Correctness_Skeleton(Java)/HearthstoneJava/src/BoardImpl.java
@@ -1,57 +1,104 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BoardImpl implements Board {
+    private Map<String, Card> cardMap;
 
     public BoardImpl() {
-
+        cardMap = new HashMap<>();
     }
 
     @Override
     public void draw(Card card) {
-        throw new UnsupportedOperationException();
+        String name = card.getName();
+        if (contains(name)) {
+            throw new IllegalArgumentException();
+        }
+        cardMap.put(name, card);
     }
 
     @Override
     public Boolean contains(String name) {
-        throw new UnsupportedOperationException();
+        return cardMap.containsKey(name);
     }
 
     @Override
     public int count() {
-        throw new UnsupportedOperationException();
+        return cardMap.size();
     }
 
     @Override
     public void play(String attackerCardName, String attackedCardName) {
-        throw new UnsupportedOperationException();
+        if (!cardMap.containsKey(attackerCardName) || !cardMap.containsKey(attackedCardName)) {
+            throw new IllegalArgumentException();
+        }
+        Card attacker = cardMap.get(attackerCardName);
+        Card attacked = cardMap.get(attackedCardName);
+        if (attacker.getLevel() != attacked.getLevel()) {
+            throw new IllegalArgumentException();
+        }
+        int initialHealth = attacked.getHealth();
+        if (initialHealth <= 0) {
+            return;
+        }
+        int currentHealth = initialHealth - attacker.getDamage();
+        if (currentHealth <= 0) {
+            attacker.setScore(attacker.getScore() + attacked.getLevel());
+        }
+        attacked.setHealth(currentHealth);
     }
 
     @Override
     public void remove(String name) {
-        throw new UnsupportedOperationException();
+        if (!cardMap.containsKey(name)) {
+            throw new IllegalArgumentException();
+        }
+        cardMap.remove(name);
     }
 
     @Override
     public void removeDeath() {
-        throw new UnsupportedOperationException();
+        cardMap.values().removeIf(card -> card.getHealth() <= 0);
     }
 
     @Override
     public Iterable<Card> getBestInRange(int start, int end) {
-        throw new UnsupportedOperationException();
+        return cardMap.values().stream()
+                .filter(card -> card.getScore() >= start && card.getScore() <= end)
+                .sorted(Comparator.comparingInt(Card::getLevel).reversed())
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterable<Card> listCardsByPrefix(String prefix) {
-        throw new UnsupportedOperationException();
+        return cardMap.values().stream()
+                .filter(card -> card.getName().startsWith(prefix))
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterable<Card> searchByLevel(int level) {
-        throw new UnsupportedOperationException();
+        return cardMap.values().stream()
+                .filter(card -> card.getLevel() == level)
+                .sorted(Comparator.comparingInt(Card::getScore).reversed())
+                .collect(Collectors.toList());
     }
 
     @Override
     public void heal(int health) {
-        throw new UnsupportedOperationException();
+        int lowestHealth = Integer.MAX_VALUE;
+        Card lowestHPCard = null;
+        for (Card card : cardMap.values()) {
+            int currentCardHealth = card.getHealth();
+            if (currentCardHealth < lowestHealth) {
+                lowestHealth = currentCardHealth;
+                lowestHPCard = card;
+            }
+        }
+        assert lowestHPCard != null;
+        lowestHPCard.setHealth(lowestHealth + health);
     }
 }
--- 02. Hearthstone - Correctness_Skeleton(Java)/HearthstoneJava/src/Card.java
+++ 02. Hearthstone - Correctness_Skeleton(Java)/HearthstoneJava/src/Card.java
@@ -1,4 +1,6 @@
-public class Card {
+import java.util.Objects;
+
+public class Card implements Comparable<Card> {
 
     private String name;
     private int score;
@@ -52,5 +54,31 @@ public class Card {
 
     public void setLevel(int level) {
         this.level = level;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Card)) {
+            return false;
+        }
+        Card card = (Card) o;
+        return Objects.equals(getName(), card.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
+
+    @Override
+    public int compareTo(Card other) {
+        int result = new StringBuilder(getName()).reverse().toString().compareTo(new StringBuilder(other.getName()).reverse().toString());
+        if (result == 0) {
+            result = getLevel() - other.getLevel();
+        }
+        return result;
     }
 }
