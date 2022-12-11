4a64a8718ff52849384cc9fc2da02474c1db017d
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-12-11 08:16:18 +0200

    add new exam

--- 01. Scheduler_Skeleton/src/main/java/Main.java
+++ 01. Scheduler_Skeleton/src/main/java/Main.java
@@ -1,5 +1,26 @@
+import core.ProcessScheduler;
+import model.ScheduledTask;
+import model.Task;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        ProcessScheduler processScheduler =new ProcessScheduler();
+        Task task = new ScheduledTask(111,"1");
+        Task task1 = new ScheduledTask(1,"1");
+        Task task2 = new ScheduledTask(2,"1");
+        Task task3 = new ScheduledTask(3,"1");
+        Task task4 = new ScheduledTask(4,"1");
+        Task task5 = new ScheduledTask(5,"1");
+        Task task6 = new ScheduledTask(6,"1");
+        processScheduler.add(task1);
+        processScheduler.add(task2);
+        processScheduler.add(task3);
+        processScheduler.add(task4);
+        processScheduler.add(task5);
+        processScheduler.add(task6);
 
+        processScheduler.insertAfter(task6.getId(),task);
     }
 }
--- 01. Scheduler_Skeleton/src/main/java/core/ProcessScheduler.java
+++ 01. Scheduler_Skeleton/src/main/java/core/ProcessScheduler.java
@@ -4,87 +4,152 @@ import model.Task;
 import shared.Scheduler;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ProcessScheduler implements Scheduler {
+    private ArrayDeque<Task> scheduledTasks;
+
+    public ProcessScheduler() {
+        this.scheduledTasks = new ArrayDeque<>();
+    }
 
     @Override
     public void add(Task task) {
-
+        if (contains(task)) {
+            throw new IllegalArgumentException();
+        }
+        scheduledTasks.offer(task);
     }
 
     @Override
     public Task process() {
-        return null;
+        return scheduledTasks.poll();
     }
 
     @Override
     public Task peek() {
-        return null;
+        return scheduledTasks.peek();
     }
 
     @Override
     public Boolean contains(Task task) {
-        return null;
+        return scheduledTasks.contains(task);
     }
 
     @Override
     public int size() {
-        return 0;
+        return scheduledTasks.size();
     }
 
     @Override
     public Boolean remove(Task task) {
-        return null;
+        boolean successful = scheduledTasks.remove(task);
+
+        if (!successful) {
+            throw new IllegalArgumentException();
+        }
+
+        return true;
     }
 
     @Override
     public Boolean remove(int id) {
-        return null;
+        boolean successful = scheduledTasks.removeIf(task -> task.getId() == id);
+
+        if (!successful) {
+            throw new IllegalArgumentException();
+        }
+
+        return true;
     }
 
     @Override
     public void insertBefore(int id, Task task) {
-
+        if (scheduledTasks.stream().noneMatch(t -> t.getId() == id)) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < scheduledTasks.size(); i++) {
+            Task poll = scheduledTasks.poll();
+            assert poll != null;
+            if (poll.getId() == id) {
+                scheduledTasks.offer(task);
+                scheduledTasks.offer(poll);
+                i++;
+            }else {
+                scheduledTasks.offer(poll);
+            }
+        }
     }
 
     @Override
     public void insertAfter(int id, Task task) {
-
+        if (scheduledTasks.stream().noneMatch(t -> t.getId() == id)) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < scheduledTasks.size(); i++) {
+            Task poll = scheduledTasks.poll();
+            assert poll != null;
+            if (poll.getId() == id) {
+                scheduledTasks.offer(poll);
+                scheduledTasks.offer(task);
+                i++;
+            } else {
+                scheduledTasks.offer(poll);
+            }
+        }
     }
 
     @Override
     public void clear() {
-
+        scheduledTasks.clear();
     }
 
     @Override
     public Task[] toArray() {
-        return new Task[0];
+        return scheduledTasks.toArray(new Task[0]);
     }
 
     @Override
     public void reschedule(Task first, Task second) {
-
+        if (!contains(first) || !contains(second)) {
+            throw new IllegalArgumentException();
+        }
+        List<Task> tasks = toList();
+        Collections.swap(tasks,tasks.indexOf(first),tasks.indexOf(second));
+        scheduledTasks.clear();
+        tasks.forEach(task -> scheduledTasks.offer(task));
     }
 
     @Override
     public List<Task> toList() {
-        return null;
+        return new ArrayList<>(scheduledTasks);
     }
 
     @Override
     public void reverse() {
-
+        List<Task> tasks = toList();
+        Collections.reverse(tasks);
+        scheduledTasks.clear();
+        tasks.forEach(task -> scheduledTasks.offer(task));
     }
 
     @Override
     public Task find(int id) {
-        return null;
+        Task taskToReturn = scheduledTasks.stream().filter(t -> t.getId() == id).findAny().orElse(null);
+        if (taskToReturn == null) {
+            throw new IllegalArgumentException();
+        }
+        return taskToReturn;
     }
 
     @Override
     public Task find(Task task) {
-        return null;
+        Task taskToReturn = scheduledTasks.stream().filter(t -> t.getId() == task.getId()).findAny().orElse(null);
+        if (taskToReturn == null) {
+            throw new IllegalArgumentException();
+        }
+        return taskToReturn;
     }
 }
