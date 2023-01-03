c9d64b1f4cc8e99ba3f4dc6a73f718dc3c070003
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-01-03 08:29:58 +0200

    add new exercise

---
+++ src/main/java/main/Hierarchy.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.util.Iterator;
+
+public class Hierarchy<T> implements IHierarchy<T> {
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public void add(T element, T child) {
+
+    }
+
+    @Override
+    public void remove(T element) {
+
+    }
+
+    @Override
+    public Iterable<T> getChildren(T element) {
+        return null;
+    }
+
+    @Override
+    public T getParent(T element) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(T element) {
+        return false;
+    }
+
+    @Override
+    public Iterable<T> getCommonElements(IHierarchy<T> other) {
+        return null;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public T next() {
+                return null;
+            }
+        };
+    }
+}
