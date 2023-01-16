04a4a5a38396a00bd23cfc1788e120b99e60fa7a
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-01-16 16:59:31 +0200

    add new exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,68 +1,64 @@
-import java.util.*;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    private static Map<String, Integer> wordsByCounts = new HashMap<>();
-    private static Map<Integer, TreeSet<String>> tree = new HashMap<>();
-    private static String target;
-    private static List<String> buffer = new ArrayList<>();
-    private static List<String> out = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        ShoppingCentre shoppingCentre = new ShoppingCentre();
+        StringBuilder builder = new StringBuilder();
+        int numberOfCommands = Integer.parseInt(reader.readLine());
 
-        List<String> inputStrings = Arrays.stream(scan.nextLine().split(", ")).collect(Collectors.toList());
+        for (int i = 0; i < numberOfCommands; i++) {
+            String commandParams = reader.readLine();
+            String command = commandParams.substring(0, commandParams.indexOf(" "));
+            String paramsString = commandParams.substring(commandParams.indexOf(" ") + 1);
+            String[] params = paramsString.split(";");
 
-        target = scan.nextLine();
-
-        inputStrings = inputStrings.stream().filter(s -> target.contains(s)).collect(Collectors.toList());
-
-        for (String string : inputStrings) {
-            wordsByCounts.putIfAbsent(string, 0);
-            wordsByCounts.put(string, wordsByCounts.get(string) + 1);
-
-            int index = target.indexOf(string);
-
-            while (index != -1) {
-                if (!tree.containsKey(index)) {
-                    tree.put(index, new TreeSet<>());
-                }
-                tree.get(index).add(string);
-                index = target.indexOf(string, index + 1);
-            }
+            executeCommand(shoppingCentre, builder, command, params);
         }
-        dfsTraversal(0);
 
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String s : out) {
-            stringBuilder.append(s).append(System.lineSeparator());
-        }
-        System.out.println(stringBuilder.toString().trim());
+        System.out.println(builder.toString());
     }
 
-    private static void dfsTraversal(int index) {
-        if (index >= target.length()) {
-            printResult();
-        } else {
-            if (!tree.containsKey(index)) {
-                return;
+    private static void executeCommand(ShoppingCentre shoppingCentre, StringBuilder builder, String command, String[] params) {
+        switch (command) {
+            case "AddProduct": {
+                String name = params[0];
+                double price = Double.parseDouble(params[1]);
+                String producer = params[2];
+                builder.append(shoppingCentre.addProduct(name, price, producer));
+                break;
             }
-            for (String str : tree.get(index)) {
-                if (wordsByCounts.get(str) > 0) {
-                    buffer.add(str);
-                    wordsByCounts.put(str, wordsByCounts.get(str) - 1);
-                    dfsTraversal(index + str.length());
-                    wordsByCounts.put(str, wordsByCounts.get(str) + 1);
-                    buffer.remove(buffer.size() - 1);
+            case "DeleteProducts": {
+                if (params.length > 1) {
+                    String name = params[0];
+                    String producer = params[1];
+                    builder.append(shoppingCentre.delete(name, producer));
+                } else {
+                    String producer = params[0];
+                    builder.append(shoppingCentre.delete(producer));
                 }
+                break;
             }
-        }
-    }
-
-    private static void printResult() {
-        String result = String.join("", buffer);
-        if (result.equals(target)) {
-            out.add(String.join(" ", buffer));
+            case "FindProductsByName": {
+                String name = params[0];
+                builder.append(shoppingCentre.findProductsByName(name));
+                break;
+            }
+            case "FindProductsByProducer": {
+                String producer = params[0];
+                builder.append(shoppingCentre.findProductsByProducer(producer));
+                break;
+            }
+            case "FindProductsByPriceRange":
+                double priceFrom = Double.parseDouble(params[0]);
+                double priceTo = Double.parseDouble(params[1]);
+                builder.append(shoppingCentre.findProductsByPriceRange(priceFrom, priceTo));
+                break;
+            default:
+                break;
         }
     }
 }
